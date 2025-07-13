ca3b1db830b9a905c8355806045d64161b158f29
Alam <mahmood_alam@dell.com>
2025-07-13 10:00:46 -0500

    09 projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,34 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useTRPC } from '@/trpc/client';
 import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
 
 const Page = () => {
+  const router = useRouter();
   const [value, setValue] = useState("");
 
   const trpc = useTRPC(); 
 
-  const createMessage = useMutation(trpc.messages.create.mutationOptions({
-    onSuccess: () => {
-      toast.success("Message created");
-    }
+  const createProject = useMutation(trpc.projects.create.mutationOptions({
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      router.push(`/projects/${data.id}`);
+    },
   }));
 
   return (
-    <div className="p-4 max-w-7xl mx-auto">
-      <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button disabled={createMessage.isPending} onClick={() => createMessage.mutate({ value: value })}>
-        Invoke Background Job
-      </Button>
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button
+          disabled={createProject.isPending}
+          onClick={() => createProject.mutate({ value: value })}
+        >
+          Submit
+        </Button>
+      </div>
     </div>
   );
 };
--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -2,43 +2,49 @@ import { inngest } from "@/inngest/client";
 import { prisma } from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
+import { generateSlug } from "random-word-slugs"
 
-export const messagesRouter = createTRPCRouter({
+export const projectsRouter = createTRPCRouter({
   getMany: baseProcedure
   .query(async () => {
-    const messages = await prisma.message.findMany({
+    const projects = await prisma.project.findMany({
       orderBy: {
         updatedAt: "desc",
       },
     });
 
-    return messages;
+    return projects;
   }),
   create: baseProcedure
     .input(
       z.object({
         value: z.string().min(1, { message: "Message is required" }),
-        projectId: z.string().min(1, { message: "Project ID is required" }),
       }),
     )
     .mutation(async ({ input }) => {
-      const createdMessage = await prisma.message.create({
+      const createdProject = await prisma.project.create({
         data: {
-          projectId: input.projectId,
-          content: input.value,
-          role: "USER",
-          type: "RESULT"
+            name: generateSlug(2, {
+            format: "kebab",
+            }),
+            messages: {
+            create: {
+                content: input.value,
+                role: "USER",
+                type: "RESULT",
+            },
+            },
         },
-      });
+        });
 
       await inngest.send({
         name: "code-agent/run",
         data: {
             value: input.value,
-            projectId: input.projectId,
+            projectId: createdProject.id
         },
       });
 
-      return createdMessage;
+      return createdProject;
     }),
 });
