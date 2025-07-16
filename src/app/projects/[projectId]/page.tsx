eb9069b58241afb20401b805616d88623961ae3a
Alam <mahmood_alam@dell.com>
2025-07-16 15:18:41 -0500

    10. messages ui

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,3 +1,9 @@
+import { ProjectView } from "@/modules/projects/ui/views/project-view";
+import { useTRPC } from "@/trpc/client";
+import { getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 interface Props {
   params: Promise<{
     projectId: string;
@@ -7,10 +13,24 @@ interface Props {
 const Page = async ({ params }: Props) => {
   const { projectId } = await params;
 
+  const trpc = useTRPC(); 
+
+  const queryClient = getQueryClient();
+  
+  void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({
+    projectId: projectId
+  }));
+
+  void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({
+    id: projectId
+  }));
+
   return (
-    <div>
-      Project id: {projectId}
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ProjectView projectId={projectId} />
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
